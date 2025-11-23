41c1408d884220f6bfc00fe8df55b60d856b36ff
nasibaNS <joldoshbekovanasiba31.com>
2025-11-23 17:31:50 +0600

    Загрузила модель. Создала папку для хр-я видео. Открыла доступ к камере и задала окно для отображения видео с размером

--- main.py
+++ main.py
@@ -2,3 +2,16 @@ from ultralytics import YOLO
 import cv2
 import time, datetime
 import os
+
+model = YOLO("yolov8n.pt")
+
+video_path = 'videos'
+os.makedirs(video_path, exist_ok=True)
+
+img_path = 'media'
+os.makedirs(img_path, exist_ok=True)
+
+cap = cv2.VideoCapture(0)
+
+cv2.namedWindow("Video", cv2.WINDOW_NORMAL)
+cv2.resizeWindow("Video", 640, 360)
